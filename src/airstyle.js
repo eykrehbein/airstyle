166991a479950987e0fafd5fae0672fc0d4e829e
Yiin- <stanislovas.janonis@gmail.com>
2018-01-06 02:33:02 +0200

    Follow airbnb javascript style guide.
    https://github.com/airbnb/javascript

--- src/airstyle.js
+++ src/airstyle.js
@@ -1,15 +1,18 @@
 const style = (element, style) => {
-	element = document.querySelector(element.toString());
-	if(typeof style === "string"){
-		element.setAttribute("style", style);
-	}
-	if(typeof style === "object"){
-		let stringified = "";
-		for(let declaration in style){
-			let value = style[declaration];
-			stringified = stringified + declaration + ":" + value + ";";
-		}
-		element.setAttribute("style", stringified);
-	}
+    element = document.querySelector(element.toString());
+
+    if (typeof style === 'string') {
+        element.setAttribute('style', style);
+    }
+
+    if (typeof style === 'object') {
+        let stringified = '';
+
+        for (let declaration in style) {
+            const value = style[declaration];
+            stringified += `${declaration}:${value};`;
+        }
+        element.setAttribute('style', stringified);
+    }
 }
-module.exports = style;
+export default style;
